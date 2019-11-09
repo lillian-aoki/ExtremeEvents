aac91ee608638a0be628ffa0e9de88eff7ff861d
Lillian Aoki <lra53@cornell.edu>
2019-11-09 11:17:47 -0800

    Plot of reasons for excludsions

---
+++ ExcludedPapers.R
@@ -0,0 +1,68 @@
+## Looking at excluded papers
+library(tidyverse)
+
+Excluded <- read.csv("Excluded papers.csv")
+Excluded2 <- read.csv("Excluded papers - second round.csv")
+Excluded <- bind_rows(Excluded,Excluded2)
+Excluded$Reason <- str_extract(Excluded$Manual.Tags,regex("(exclude).+$|(Exclude).+$"))
+
+# cleaning up levels 
+Excluded$Reason <- gsub("because","-",Excluded$Reason)
+Excluded$Reason <- gsub("Exclude","exclude",Excluded$Reason)
+Excluded$fReason <- as.factor(Excluded$Reason)
+levels(Excluded$fReason)
+
+## broader categories
+Excluded$Reason2 <- factor(rep(NA,length(Excluded$Reason)),
+                           levels=c("Review","Terrestrial","Non-empirical","No extreme event",
+                                    "Paleo","Not ecology","Other","Experimental"))
+Excluded$Reason2[Excluded$Reason %in% c("excluded - review",
+                                        "excluded - review, book; duplicate",
+                                        "excluded - book chapter/review",
+                                        "excluded - review, book"
+                                        )] <- "Review"
+Excluded$Reason2[Excluded$Reason %in% c("excluded - terrestrial food webs",
+                                        "excluded - terrestrial, fauna",
+                                        "excluded - terrestrial, human disease",
+                                        "excluded - terrestrial vegetation",
+                                        "excluded - terrestrial, forests",
+                                        "excluded - not aquatic"
+                                        )] <- "Terrestrial"
+Excluded$Reason2[Excluded$Reason %in% c("exclude- theoretical framework, not raw data",
+                                        "excluded - modeling",
+                                        "exclude - mass balance model",
+                                        "excluded- no new ecological data"
+                                        )] <- "Non-empirical"
+Excluded$Reason2[Excluded$Reason %in% c("exclude - climate changes",
+                                        "exclude - long term monitoring",
+                                        "excluded - long-term trends",
+                                        "exclude - not extreme event",
+                                        "exclude- human stressor, but no extreme climate event",
+                                        "excluded - about how ecosystem affects extreme events",
+                                        "excluded - does not describe extreme event",
+                                        "excluded - not about extreme events; excluded - review",
+                                        "excluded - does not describe extreme event; excluded - review",
+                                        "excluded - not about extreme events",
+                                        "excluded - not extreme event"
+                                        )] <- "No extreme event"
+Excluded$Reason2[Excluded$Reason %in% c("excluded - ancient record (Phanerozoic)",
+                                        "excluded - not modern",
+                                        "exclude - historical",
+                                        "excluded - ancient record (Holocene)",
+                                        "excluded - historical data",
+                                        "excluded - paleo, not modern extreme event"
+                                        )] <- "Paleo"
+Excluded$Reason2[Excluded$Reason %in% c("excluded - not about ecology",
+                                        "excluded - not ecology",
+                                        "excluded- no new ecological data"
+                                        )] <- "Not ecology"
+Excluded$Reason2[Excluded$Reason %in% c("excluded - symposium overview")] <- "Other"
+Excluded$Reason2[Excluded$Reason %in% c("excluded - experimental")] <- "Experimental"
+Excluded$Reason2 <- reorder(Excluded$Reason2,Excluded$Reason2,function(x)-length(x))
+## look at how many papers were excluded from various categories
+ggplot(Excluded,aes(x=Reason2))+geom_bar()+
+  ylab("# of papers")+
+  xlab("Reason for exclusion")+
+  theme_bw()
+#Note that this isn't quite the full list - some papers were excluded at the data entry stage and Zotero folders haven't been updated
+#If we think this is a useful piece of analysis to include, would need to update the final numbers
--- ExploratoryPlots.R
+++ ExploratoryPlots.R
@@ -2,26 +2,46 @@ library(readr)
 library(tidyverse)
 library(ggplot2)
 
-EEdata <- read.csv("EEdata.csv") #load via readr
-
+EEdata <- read_csv("EEdata.csv",skip_empty_rows = TRUE)
 EEdata <- distinct(EEdata,UniqueAccession,.keep_all = TRUE)
-EEdata <- EEdata[-52,]
-EEdata$Extreme_Definition_Num
+EEdata <- EEdata[-52,] #can't figure out a better way to get rid of the final NA row
 
-temp 
-  
-  
-EEdata$`Continuous Monitoring Prior`
-ProxEv <- EEdata %>%
-  group_by(ProximateEvent_Type)%>%
-  summarise(numPapers=length(subset(EEdata,Extreme_Definition=="Yes with a non-statistical context-specific definition")))
-  spread(key=Extreme_Definition,value=Def)
+### Adding some now data columns for more broad categories
 
-ggplot(EEdata,aes(fill=Extreme_Definition,x=ProximateEvent_Type))+geom_bar()+
+EEdata$Type_system_broad <- factor(rep(NA,length(EEdata$Type_system)),
+                                   levels=c("freshwater","coastal","marine"))
+EEdata$Type_system_broad[EEdata$Type_system %in% c("catchment/watershed","floodplain","lake","pond","stream/river","wetland")] <- "freshwater"
+EEdata$Type_system_broad[EEdata$Type_system %in% c("estuary","lagoon","marine intertidal")] <- "coastal" 
+# question if "coastal ocean" is coastal or marine - the only paper in this category looked at subtidal algae beds
+EEdata$Type_system_broad[EEdata$Type_system %in% c("coastal ocean","deep sea","open ocean")] <- "marine"
+# given there are so few marine events, maybe better to combine coastal and marine categories
+
+EEdata$ProximateEvent_Type_broad <- factor(rep(NA,length(EEdata$ProximateEvent_Type)),
+                                           levels=c("Flood","Drought","Heatwave","Other"))
+EEdata$ProximateEvent_Type_broad[EEdata$ProximateEvent_Type %in% c("Flood")] <- "Flood"
+EEdata$ProximateEvent_Type_broad[EEdata$ProximateEvent_Type %in% c("Drought")] <- "Drought"
+EEdata$ProximateEvent_Type_broad[EEdata$ProximateEvent_Type %in% c("Heatwave")] <- "Heatwave"
+EEdata$ProximateEvent_Type_broad[is.na(EEdata$ProximateEvent_Type_broad)] <- "Other"
+
+### Re-ordering the statistical definitions
+EEdata$Extreme_Definition <- as.factor(EEdata$Extreme_Definition)
+ExDef_order <- c("No - extreme event not mentioned but inferred",
+                 "No - extreme event mentioned but not defined",
+                 "Yes with a non-statistical general definition",
+                 "Yes with a non-statistical context-specific definition",
+                 "Yes with a statistical general definition",
+                 "Yes with a statistical context-specific definition")
+EEdata$Extreme_Definition <- ordered(EEdata$Extreme_Definition,levels=ExDef_order)
+### Exploratory plots
+ggplot(EEdata,aes(fill=Extreme_Definition,x=ProximateEvent_Type_broad))+geom_bar()+
+  ylab("# of papers")+
   theme_bw()
-
-ggplot(EEdata,aes(fill=Extreme_Definition,x=DistalEvent_Type))+geom_bar()
-ggplot(EEdata,aes(fill=Extreme_Definition,x=Type_system))+geom_bar()+
+ggplot(EEdata,aes(fill=Extreme_Definition,x=DistalEvent_Type))+geom_bar()+
+  ylab("# of papers")+
+  theme_bw()
+ggplot(EEdata,aes(fill=Extreme_Definition,x=Type_system_broad))+geom_bar()+
+  ylab("# of papers")+
   theme_bw()
 ggplot(EEdata,aes(x=EEdata$'Continuous Monitoring Prior'))+geom_bar()+
+  ylab("# of papers")+
   theme_bw()
